9410881bd841c0ca40ac6ef0f86608619ebbd851
volkanakbayir <volkan.akbayir@kalitte.com.tr>
2017-03-09 07:10:47 +0300

    delete dashlets multiple

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,6 @@ export class MySQLDbProvider implements IDBProvider {
         query.startFrom = parseInt(query.startFrom);
         search = search || {};
 
-        var q = <any>{};
         var filters = [];
         if (search.appid) {
             filters.push("AppId IN (?)");
@@ -58,7 +57,6 @@ export class MySQLDbProvider implements IDBProvider {
 
         if (search.user) {
             filters.push("User IN (?)");
-            (q.user = { $in: search.user });
         }
 
         var self = this;
@@ -258,9 +256,13 @@ export class MySQLDbProvider implements IDBProvider {
 
     deleteDashlet(id: string | Array<string>): Promise<any> {
 
+        if (!(id instanceof Array)) {
+            id = [id];
+        }
+
         var self = this;
         var promise = new Promise((resolve, reject) => {
-            this.connection.query('DELETE FROM dashlet WHERE Id = ?', [id], function (error, results, fields) {
+            this.connection.query('DELETE FROM dashlet WHERE Id IN (?)', [id], function (error, results, fields) {
                 if (error) {
                     reject(error);
                 } else {
